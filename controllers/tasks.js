592abf73f3fa0bcf520d3848dca56402095baeba
David-mwendwa <klatzdavie19@gmail.com>
2022-02-06 08:57:54 +0300

    add get single task and creating a task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,31 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-  res.send('all items from the file');
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.send('get single task');
+const getTask = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findById(id);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
