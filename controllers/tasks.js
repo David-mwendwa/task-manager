4a04d12a7856a321e39cc0f2256d37b1b5d77e6b
David-mwendwa <klatzdavie19@gmail.com>
2022-02-06 09:24:50 +0300

    implement task deletion

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -20,8 +20,11 @@ const createTask = async (req, res) => {
 
 const getTask = async (req, res) => {
   try {
-    const id = req.params.id;
-    const task = await Task.findById(id);
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
     res.status(201).json({ task });
   } catch (error) {
     res.status(500).json({ msg: error });
@@ -32,8 +35,13 @@ const updateTask = (req, res) => {
   res.send('update task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id: ${taskID}` });
+  }
+  res.status(200).json({ task });
 };
 
 module.exports = {
